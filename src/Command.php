1cd2aa8d5d8e13e18d725727b51dba30807e628a
Dimitri Gilbert <dimitri.gilbert@gmail.com>
2016-04-29 00:50:14 +0800

    mostly PSR2 and comments

--- src/Command.php
+++ src/Command.php
@@ -6,370 +6,416 @@ namespace D2G\Reactor;
 */
 class Command
 {
-	protected $name = 'Basic Command';
-	protected $args = array();
-	protected $opts = array();
-	protected $flags = array();
-	protected $commands = array();
-	public $cli = null;
-	
-	function __construct($args, $opts, $flags)
-	{
-		$this->cli = new \League\CLImate\CLImate;
+    protected $name = 'Basic Command';
+    protected $args = array();
+    protected $opts = array();
+    protected $flags = array();
+    protected $commands = array();
+    public $cli = null;
+    
+    /**
+     * constructor
+     * @param array $args  cli arguments
+     * @param array $opts  cli options
+     * @param array $flags cli flags
+     */
+    function __construct($args, $opts, $flags)
+    {
+        $this->cli = new \League\CLImate\CLImate;
 
-		$this->args = $args;
-		$this->opts = $opts;
-		$this->flags = $flags;
+        $this->args = $args;
+        $this->opts = $opts;
+        $this->flags = $flags;
 
-		$this->__debug('args :
-	'.print_r($args, 1));
-		$this->__debug('opts :
-	'.print_r($opts, 1));
-		$this->__debug('flags :
-	'.print_r($flags, 1));
-		
-		$this->command = null;
-		$this->commands = array(
-			'__DEFAULT__'=>'__help'
-		);
-	}
+        $this->__debug('args :
+    '.print_r($args, 1));
+        $this->__debug('opts :
+    '.print_r($opts, 1));
+        $this->__debug('flags :
+    '.print_r($flags, 1));
+        
+        $this->command = null;
+        $this->commands = array(
+            '__DEFAULT__'=>'__help'
+        );
+    }
 
-	public function getArg($index, $default = null)
-	{
-		if (count($this->args) > $index)
-		{
-			return $this->args[$index];
-		}
-		elseif ($this->getFlag('i'))
-		{
-			$str = 'Argument '.$index.' needed :
+    /**
+     * get an argument with its index in the cli input
+     * @param  integer $index
+     * @param  mixed $default optional the default value if the arg is not found
+     * @return mixed
+     */
+    public function getArg($index, $default = null)
+    {
+        if (count($this->args) > $index){
+            return $this->args[$index];
+        }
+        elseif ($this->getFlag('i')){
+            $str = 'Argument '.$index.' needed :
 ';
-			if (
-				isset($this->commands[$this->command])
-				and isset($this->commands[$this->command]['expecting']['args'])
-				and isset($this->commands[$this->command]['expecting']['args'][$index])
-			)
-			{
-				$srt = 'Argument '.$this->commands[$this->command]['expecting']['args'][$index]['name'].' needed :
+            if (
+                isset($this->commands[$this->command])
+                and isset($this->commands[$this->command]['expecting']['args'])
+                and isset($this->commands[$this->command]['expecting']['args'][$index])
+            ){
+                $srt = 'Argument '.$this->commands[$this->command]['expecting']['args'][$index]['name'].' needed :
 ';
-			}
-			$prompt = $this->cli->input($str);
-			$prompt = $prompt->prompt();
-			$this->args[$index] = $prompt;
-			return $prompt;
-		}
-		return $default;
-	}
+            }
+            $prompt = $this->cli->input($str);
+            $prompt = $prompt->prompt();
+            $this->args[$index] = $prompt;
+            return $prompt;
+        }
+        return $default;
+    }
 
-	/**
-	 *
-	 */
-	public function getArgs()
-	{
-		return $this->args;
-	}
+    /**
+     * return all the arguments of the command
+     * @return array 
+     */
+    public function getArgs()
+    {
+        return $this->args;
+    }
 
-	public function setArg($index, $value)
-	{
-		if (count($this->args) > $index)
-		{
-			$this->args[$index] = $value;
-			return true;
-		}
-		return false;
-	}
+    /**
+     * override an argument value
+     * @param integer $index
+     * @param mixed $value
+     * @return boolean
+     */
+    public function setArg($index, $value)
+    {
+        if (count($this->args) > $index) {
+            $this->args[$index] = $value;
+            return true;
+        }
+        return false;
+    }
 
-	public function setArgs($values)
-	{
-		$this->args[] = $values;
-		return true;
-	}
+    /**
+     * set the array of arguments
+     * @param array $values
+     * @return  boolean [<description>]
+     */
+    public function setArgs($values)
+    {
+        $this->args[] = $values;
+        return true;
+    }
 
-	public function getOpt($index, $default = null)
-	{
-		if (isset($this->opts[$index]))
-		{
-			return $this->opts[$index];
-		}
-		elseif ($this->getFlag('i'))
-		{
-			$prompt = $this->cli->input('Option '.$index.' needed :
+    /**
+     * get an option from the cli option input
+     * @param  string $index the name of the option
+     * @param  mixed $default the default value of the option
+     * @return mixed
+     */
+    public function getOpt($index, $default = null)
+    {
+        if (isset($this->opts[$index])) {
+            return $this->opts[$index];
+        }
+        elseif ($this->getFlag('i')) {
+            $prompt = $this->cli->input('Option '.$index.' needed :
 ');
-			$prompt = $prompt->prompt();
-			$this->opts[$index] = $prompt;
-			return $prompt;
-		}
-		return $default;
-	}
+            $prompt = $prompt->prompt();
+            $this->opts[$index] = $prompt;
+            return $prompt;
+        }
+        return $default;
+    }
 
-	/**
-	 *
-	 */
-	public function getopts()
-	{
-		return $this->opts;
-	}
+    /**
+     * get all input options
+     * @return array
+     */
+    public function getopts()
+    {
+        return $this->opts;
+    }
 
-	public function setOpt($index, $value)
-	{
-		if (isset($this->opts[$index]))
-		{
-			if (!is_array($this->opts[$index]))
-			{
-				$this->opts[$index] = array($this->opts[$index]);
-			}
-		}
-		else
-		{
-			$this->opts[$index] = $value;
-		}
-		return true;
-	}
+    /**
+     * override the value of an option
+     * @param string $index
+     * @param mixed $value
+     * @return boolean
+     */
+    public function setOpt($index, $value)
+    {
+        if (isset($this->opts[$index])) {
+            if (!is_array($this->opts[$index])) {
+                $this->opts[$index] = array($this->opts[$index]);
+            }
+        }
+        else {
+            $this->opts[$index] = $value;
+        }
+        return true;
+    }
 
-	public function setOpts($values)
-	{
-		$this->opts[] = $values;
-		return true;
-	}
+    /**
+     * set the value of the options' array
+     * @param array $values
+     */
+    public function setOpts($values)
+    {
+        $this->opts[] = $values;
+        return true;
+    }
 
-	public function getFlag($index, $default = false)
-	{
-		if (isset($this->flags[$index]))
-		{
-			return true;
-		}
-		return $default;
-	}
+    /**
+     * get a flag from the cli input
+     * @param  string  $index
+     * @param  boolean $default
+     * @return boolean
+     */
+    public function getFlag($index, $default = false)
+    {
+        if (isset($this->flags[$index])) {
+            return true;
+        }
+        return $default;
+    }
 
-	/**
-	 *
-	 */
-	public function getFlags()
-	{
-		return $this->flags;
-	}
+    /**
+     * get all flags fomr cli input
+     * @return array
+     */
+    public function getFlags()
+    {
+        return $this->flags;
+    }
 
-	public function setFlag($index, $value = true)
-	{
-		$this->flags[$index] = true;
-		if (!$value)
-		{
-			unset($this->flags[$index]);
-		}
-		
-		return true;
-	}
+    /**
+     * override a flag
+     * @param string  $index
+     * @param boolean $value
+     */
+    public function setFlag($index, $value = true)
+    {
+        $this->flags[$index] = true;
+        if (!$value) {
+            unset($this->flags[$index]);
+        }
+        
+        return true;
+    }
 
-	public function hasExpected($function)
-	{
-		$checker = $this->commands[$function];
-		if (isset($this->commands[$function]['expecting']['args']))
-		{
-			foreach ($this->commands[$function]['expecting']['args'] as $index => $arg)
-			{
-				if (is_null($this->getArg($index)) and $this->getFlag('ii'))
-				{
-					$this->getArg($index);
-				}
-				elseif (is_null($this->getArg($index)) and isset($arg['required']) and $arg['required'])
-				{
-					return false;
-				}
-			}
-		}
-		if (isset($this->commands[$function]['expecting']['opts']))
-		{
-			foreach ($this->commands[$function]['expecting']['opts'] as $k => $opt)
-			{
-				if (is_null($this->getOpt($k)) and isset($opt['default']))
-				{
-					$this->opts[$k] = $opt['default'];
-				}
-				elseif (is_null($this->getOpt($k)) and $this->getFlag('ii'))
-				{
-					$this->getOpt($k);
-				}
-				elseif (is_null($this->getOpt($k)) and isset($opt['required']) and $opt['required'])
-				{
-					return false;
-				}
-			}
-		}
-		return true;
-	}
+    /**
+     * check if the called function has expected argument
+     * @param  string  $function
+     * @return boolean
+     */
+    protected function hasExpected($function)
+    {
+        $checker = $this->commands[$function];
+        if (isset($this->commands[$function]['expecting']['args'])) {
+            foreach ($this->commands[$function]['expecting']['args'] as $index => $arg) {
+                if (is_null($this->getArg($index)) and $this->getFlag('ii')) {
+                    $this->getArg($index);
+                }
+                elseif (is_null($this->getArg($index)) and isset($arg['required']) and $arg['required']) {
+                    return false;
+                }
+            }
+        }
+        if (isset($this->commands[$function]['expecting']['opts'])) {
+            foreach ($this->commands[$function]['expecting']['opts'] as $k => $opt) {
+                if (is_null($this->getOpt($k)) and isset($opt['default'])) {
+                    $this->opts[$k] = $opt['default'];
+                }
+                elseif (is_null($this->getOpt($k)) and $this->getFlag('ii')) {
+                    $this->getOpt($k);
+                }
+                elseif (is_null($this->getOpt($k)) and isset($opt['required']) and $opt['required']) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
-	public function __execute($function = null)
-	{
-		if (is_null($function))
-		{
-			$function = $this->commands['__DEFAULT__'];
-		}
+    /**
+     * execute a command
+     * @param  string $function the method name
+     * @return mixed
+     */
+    public function __execute($function = null)
+    {
+        if (is_null($function)) {
+            $function = $this->commands['__DEFAULT__'];
+        }
 
-		$this->__debug('executing '.$function);
+        $this->__debug('executing '.$function);
 
-		if (isset($this->commands[$function]))
-		{
-			if (isset($thic->commands[$function]['alias']))
-			{
-				$function = $thic->commands[$function]['alias'];
-			}
+        if (isset($this->commands[$function])) {
+            if (isset($thic->commands[$function]['alias'])) {
+                $function = $thic->commands[$function]['alias'];
+            }
 
-			if (!$this->hasExpected($function))
-			{
-				$this->cli->backgroundRed()->white()->out('Missing inputs...');
-				$this->__help();
-				return false;
-			}
-		}
+            if (!$this->hasExpected($function)) {
+                $this->cli->backgroundRed()->white()->out('Missing inputs...');
+                $this->__help();
+                return false;
+            }
+        }
 
-		if (method_exists($this, $function))
-		{
-			$this->command = $function;
-			try 
-			{
-				$this->$function();
-			}
-			catch (Exception $e)
-			{
-				$this->cli->backgroundRed()->white()->dump($e);
-			}
-		}
-	}
+        if (method_exists($this, $function)) {
+            $this->command = $function;
+            try {
+                $this->$function();
+            }
+            catch (\Exception $e) {
+                $this->cli->backgroundRed()->white()->dump($e);
+            }
+        }
+        else {
+            $this->cli->backgroundRed()->white()->out($function.' does not exist !');
+        }
+    }
 
-	public function __help()
-	{
-		$this->cli->out($this->name);
-		$this->cli->out('Usage');
-		foreach ($this->commands as $command => $data)
-		{
-			if ($command !== '__DEFAULT__')
-			{
-				$line = ':'.$command;
-				if (isset($data['expecting']) and !is_null($data['expecting']))
-				{
-					if (isset($data['expecting']['args']))
-					{
-						foreach ($data['expecting']['args'] as $arg)
-						{
-							if (isset($arg['required']) and $arg['required'] === true)
-							{
-								$line .= '
-	<';
-							}
-							else
-							{
-								$line .= '
-	[';
-							}
+    /**
+     * display the class help
+     */
+    public function __help()
+    {
+        $this->cli->out($this->name);
+        $this->cli->out('Usage');
+        foreach ($this->commands as $command => $data) {
+            if ($command !== '__DEFAULT__') {
+                $line = ':'.$command;
+                if (isset($data['expecting']) and !is_null($data['expecting'])) {
+                    if (isset($data['expecting']['args'])) {
+                        foreach ($data['expecting']['args'] as $arg) {
+                            if (isset($arg['required']) and $arg['required'] === true) {
+                                $line .= '
+    <';
+                            }
+                            else {
+                                $line .= '
+    [';
+                            }
 
-							$line .= $arg['name'].':'.$arg['type'];
+                            $line .= $arg['name'].':'.$arg['type'];
 
-							if (isset($arg['required']) and $arg['required'] === true)
-							{
-								$line .= '>';
-							}
-							else
-							{
-								$line .= ']';
-							}
-						}
-					}
-					if (isset($data['expecting']['opts']))
-					{
-						foreach ($data['expecting']['opts'] as $key => $opt)
-						{
-							if (isset($opt['required']) and $opt['required'] === true)
-							{
-								$line .= '
-	<';
-							}
-							else
-							{
-								$line .= '
-	[';
-							}
+                            if (isset($arg['required']) and $arg['required'] === true) {
+                                $line .= '>';
+                            }
+                            else {
+                                $line .= ']';
+                            }
+                        }
+                    }
+                    if (isset($data['expecting']['opts'])) {
+                        foreach ($data['expecting']['opts'] as $key => $opt) {
+                            if (isset($opt['required']) and $opt['required'] === true) {
+                                $line .= '
+    <';
+                            }
+                            else {
+                                $line .= '
+    [';
+                            }
 
-							$line .= '--'.$key./*'='.isset($opt['default'])?$opt['default']:''.*/':'.$opt['type'];
+                            $line .= '--'.$key./*'='.isset($opt['default'])?$opt['default']:''.*/':'.$opt['type'];
 
-							if (isset($opt['required']) and $opt['required'] === true)
-							{
-								$line .= '>';
-							}
-							else
-							{
-								$line .= ']';
-							}
-						}
-					}
-				}
-				$this->cli->green($line);
-			}
-		}
-	}
+                            if (isset($opt['required']) and $opt['required'] === true) {
+                                $line .= '>';
+                            }
+                            else {
+                                $line .= ']';
+                            }
+                        }
+                    }
+                }
+                $this->cli->green($line);
+            }
+        }
+    }
 
-	public function __out($str)
-	{
-		if (!$this->getFlag('q'))
-		{
-			$this->cli->out($str);
-		}
-	}
+    /**
+     * output a string to the terminal
+     * @param  string $str the string to output
+     */
+    public function __out($str)
+    {
+        if (!$this->getFlag('q')) {
+            $this->cli->out($str);
+        }
+    }
 
-	public function __error($str)
-	{
-		$this->cli->backgroundRed($str);
-	}
+    /**
+     * output a string to the terminal as an error message
+     * @param  string $str the string to output
+     */
+    public function __error($str)
+    {
+        $this->cli->backgroundRed($str);
+    }
 
-	/**
-	 *
-	 */
-	public function __success($str)
-	{
-		$this->cli->green($str);
-	}
+    /**
+     * output a string to the terminal as a success message
+     * @param  string $str the string to output
+     */
+    public function __success($str)
+    {
+        $this->cli->green($str);
+    }
 
-	/**
-	 *
-	 */
-	public function __notice($str)
-	{
-		$this->cli->blue($str);
-	}
+    /**
+     * output a string to the terminal as a notice
+     * @param  string $str the string to output
+     */
+    public function __notice($str)
+    {
+        $this->cli->blue($str);
+    }
 
-	/**
-	 *
-	 */
-	public function __warning($str)
-	{
-		$this->cli->backgroundYellow()->black($str);
-	}
+    /**
+     * output a string to the terminal as a warning
+     * @param  string $str the string to output
+     */
+    public function __warning($str)
+    {
+        $this->cli->backgroundYellow()->black($str);
+    }
 
-	public function __verbose($str)
-	{
-		if (
-			$this->getFlag('v')
-			or $this->getFlag('verbose')
-			or $this->getFlag('vv')
-			or $this->getFlag('debug')
-		)
-		{
-			$this->cli->out($str);
-		}
-	}
+    /**
+     * output a string to the terminal if verbose is active
+     * @param  string $str the string to output
+     */
+    public function __verbose($str)
+    {
+        if (
+            $this->getFlag('v')
+            or $this->getFlag('verbose')
+            or $this->getFlag('vv')
+            or $this->getFlag('debug')
+        ) {
+            $this->cli->out($str);
+        }
+    }
 
-	public function __verbose2($str)
-	{
-		if ($this->getFlag('vv') or $this->getFlag('debug'))
-		{
-			$this->cli->out($str);
-		}
-	}
+    /**
+     * output a string to the terminal if very verbose is active
+     * @param  string $str the string to output
+     */
+    public function __verbose2($str)
+    {
+        if ($this->getFlag('vv') or $this->getFlag('debug')) {
+            $this->cli->out($str);
+        }
+    }
 
-	public function __debug($str)
-	{
-		if ($this->getFlag('debug'))
-		{
-			$this->cli->backgroundYellow()->black()->out($str);
-		}
-	}
+    /**
+     * output a string to the terminal if debug is active
+     * @param  string $str the string to output
+     */
+    public function __debug($str)
+    {
+        if ($this->getFlag('debug')) {
+            $this->cli->backgroundYellow()->black()->out($str);
+        }
+    }
 }
--- src/Reactor.php
+++ src/Reactor.php
@@ -6,152 +6,153 @@ namespace D2G\Reactor;
 */
 class Reactor
 {
-	protected $args = array();
-	protected $opts = array();
-	protected $flags = array();
-	protected $command = null;
-	protected $callback = null;
-	protected $config = null;
-	public $_command = null;
-	
-	function __construct($argv)
-	{
-		if (!is_file(APPPATH.'/reactor.json'))
-		{
-			file_put_contents(APPPATH.'/reactor.json', json_encode(array(
-				'namespaces'=>array(
-					'D2G\\Reactor\\',
-					'D2G\\Reactor\\Commands\\'
-				)
-			)));
-		}
+    protected $args = array();
+    protected $opts = array();
+    protected $flags = array();
+    protected $command = null;
+    protected $callback = null;
+    protected $config = array();
+    public $_command = null;
+    
+    /**
+     * constructor
+     * @param array $argv command line input array
+     */
+    function __construct($argv)
+    {
+        // if (!is_file(APPPATH.'/reactor.json'))
+        // {
+        //  file_put_contents(APPPATH.'/reactor.json', json_encode(array(
+        //      'namespaces'=>array(
+        //          'D2G\\Reactor\\',
+        //          'D2G\\Reactor\\Commands\\'
+        //      )
+        //  )));
+        // }
+        if (is_file(APPPATH.'/reactor.json')) {
+            $this->config = json_decode(file_get_contents(APPPATH.'/reactor.json'), 1);
+        }
 
-		$this->config = json_decode(file_get_contents(APPPATH.'/reactor.json'), 1);
+        if (!isset($argv[1]) or $argv[1] === '') {
+            $argv[1] = 'Command';
+        }
+        
+        $_command = preg_split('#\:#', $argv[1]);
 
-		if (!isset($argv[1]) or $argv[1] === '')
-		{
-			$argv[1] = 'Command';
-		}
-		
-		$_command = preg_split('#\:#', $argv[1]);
+        if (count($_command) > 1) {
+            $this->callback = $_command[1];
+        }
 
-		if (count($_command) > 1)
-		{
-			$this->callback = $_command[1];
-		}
+        if (preg_match('#\/#', $_command[0])) {
+            $_command[0] = preg_replace('#\/\/#', '/Commands/', $_command[0]);
+            $_command[0] = preg_replace('#\/#', '\\', $_command[0]);
+        }
 
-		if (preg_match('#\/#', $_command[0]))
-		{
-			$_command[0] = preg_replace('#\/\/#', '/Commands/', $_command[0]);
-			$_command[0] = preg_replace('#\/#', '\\', $_command[0]);
-		}
+        $this->_command = $this->getAllias($_command[0]);
 
-		$this->_command = $this->getAllias($_command[0]);
+        $argv = array_slice($argv, 2);
 
-		$argv = array_slice($argv, 2);
+        // parse given cli arg
+        foreach ($argv as $arg) {
+            // matching option
+            if (preg_match('#^--#', $arg)) {
+                $arg = preg_replace('#--#', '', $arg);
+                $arg = preg_split('#=#', $arg);
+                if (!isset($this->opts[$arg[0]])) {
+                    $this->opts[$arg[0]] = true;
 
-		foreach ($argv as $arg)
-		{
-			if (preg_match('#^--#', $arg))
-			{
-				$arg = preg_replace('#--#', '', $arg);
-				$arg = preg_split('#=#', $arg);
-				if (!isset($this->opts[$arg[0]]))
-				{
-					$this->opts[$arg[0]] = true;
+                    if (count($arg) > 1) {
+                        $this->opts[$arg[0]] = $arg[1];
+                    }
+                }
+                else {
+                    if (!is_array($this->opts[$arg[0]])) {
+                        $this->opts[$arg[0]] = array($this->opts[$arg[0]]);
+                    }
 
-					if (count($arg) > 1)
-					{
-						$this->opts[$arg[0]] = $arg[1];
-					}
-				}
-				else
-				{
-					if (!is_array($this->opts[$arg[0]]))
-					{
-						$this->opts[$arg[0]] = array($this->opts[$arg[0]]);
-					}
+                    if (count($arg) > 1) {
+                        $this->opts[$arg[0]][] = $arg[1];
+                    }
+                    else {
+                        $this->opts[$arg[0]][] = true;
+                    }
+                }
+            }
+            // matching flags
+            elseif (preg_match('#^-#', $arg)) {
+                $arg = preg_replace('#-#', '', $arg);
+                $this->flags[$arg] = true;
+            }
+            // everything else goes in as an argument
+            else {
+                $this->args[] = $arg;
+            }
+        }
 
-					if (count($arg) > 1)
-					{
-						$this->opts[$arg[0]][] = $arg[1];
-					}
-					else
-					{
-						$this->opts[$arg[0]][] = true;
-					}
-				}
-			}
-			elseif (preg_match('#^-#', $arg))
-			{
-				$arg = preg_replace('#-#', '', $arg);
-				$this->flags[$arg] = true;
-			}
-			else
-			{
-				$this->args[] = $arg;
-			}
-		}
+        $class = $this->getClass();
+        if ($class === false) {
+            throw new \Exception("Unknown Class : ".$this->_command, 1);
+            
+        }
+        $this->command = new $class($this->args, $this->opts, $this->flags);
 
-		$class = $this->getClass();
-		if ($class === false)
-		{
-			throw new \Exception("Unknown Class : ".$this->_command, 1);
-			
-		}
-		$this->command = new $class($this->args, $this->opts, $this->flags);
+        return true;
+    }
 
-		return true;
-	}
+    /**
+     * get a method alias defined in the config file
+     * @param  string $command 
+     * @return string
+     */
+    public function getAllias($command)
+    {
+        if (
+            isset($this->config['alliases'])
+            and in_array($command, $this->config['alliases'])
+        ) {
+            $data = $this->config['alliases'][$command];
+            $command = $data['command'];
+            if (
+                is_null($this->callback)
+                and isset($data['callback'])
+            ) {
+                $this->callback = $data['callback'];
+            }
+        }
 
-	public function getAllias($command)
-	{
-		if (
-			isset($this->config['alliases'])
-			and in_array($command, $this->config['alliases'])
-		)
-		{
-			$data = $this->config['alliases'][$command];
-			$command = $data['command'];
-			if (
-				is_null($this->callback)
-				and isset($data['callback'])
-			)
-			{
-				$this->callback = $data['callback'];
-			}
-		}
+        return $command;
+    }
 
-		return $command;
-	}
+    /**
+     * get the class name from argument
+     * @return string
+     */
+    public function getClass()
+    {
+        $class = false;
+        if (class_exists($this->_command)) {
+            $class = $this->_command;
+        }
+        elseif (isset($this->config['namespaces'])) {
+            foreach ($this->config['namespaces'] as $namespace) {
+                if (class_exists($namespace.$this->_command)) {
+                    $class = $namespace.$this->_command;
+                }
+            }
+        }
 
-	public function getClass()
-	{
-		$class = false;
-		if (class_exists($this->_command))
-		{
-			$class = $this->_command;
-		}
-		elseif (isset($this->config['namespaces']))
-		{
-			foreach ($this->config['namespaces'] as $namespace)
-			{
-				if (class_exists($namespace.$this->_command))
-				{
-					$class = $namespace.$this->_command;
-				}
-			}
-		}
+        return $class;
+    }
 
-		return $class;
-	}
-
-	public function ignite()
-	{
-		if (!is_null($this->command))
-		{
-			return $this->command->__execute($this->callback);
-		}
-		echo 'Unknown command';
-	}
+    /**
+     * execute the command
+     * @return mixed the result of the command
+     */
+    public function ignite()
+    {
+        if (!is_null($this->command)) {
+            return $this->command->__execute($this->callback);
+        }
+        echo 'Unknown command';
+    }
 }
